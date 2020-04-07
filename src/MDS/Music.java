4d3f75b64a6c5d5b87ece4c1882dfdc4e3823f99
BitFreak <blw_matt@rocketmail.com>
2020-04-07 19:50:03 +0300

    added print function in event classes

--- src/MDS/Music.java
+++ src/MDS/Music.java
@@ -1,22 +1,36 @@
 package MDS;
 
-import MDS.*;
-
 import java.util.List;
 
 public class Music extends Event {
 
     private String formationName;
-    private List<String> membersName;
+    private List<String> members;
 
-    public Music(String name, Location location, Organizer organizer, int nrTicketTypes, Ticket[] tickets, Date startDate, String formationName, List<String> membersName) {
+    public Music(String name, Location location, Organizer organizer, int nrTicketTypes, Ticket[] tickets, Date startDate, String formationName, List<String> members) {
         super(name, location, organizer, nrTicketTypes, tickets, startDate);
         this.formationName = formationName;
-        this.membersName = membersName;
+        this.members = members;
     }
 
+    public String getFormationName() {
+        return formationName;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+
+    @Override
+    public void showMainInfo() {
+        super.showMainInfo();
+        System.out.println("Formation name: " + formationName + "\n");
+        System.out.println("Members:\n");
+        for(String member: members){
+            System.out.println(member + "\n");
+        }
+
+    }
 
     public void showRestInfo(){}
-    protected void setTicketsNumber(){}
-    protected void setPrice(){}
 }
--- src/MDS/Sport.java
+++ src/MDS/Sport.java
@@ -1,23 +1,33 @@
 package MDS;
 
-import MDS.*;
-
 import java.util.List;
 
 public class Sport extends Event {
 
-    private List<String> refereesName;
     private String homeTeam;
     private String guestTeam;
 
-    public Sport(String name, Location location, Organizer organizer, int nrTicketTypes, Ticket[] tickets, Date startDate, List<String> refereesName, String homeTeam, String guestTeam) {
+    public Sport(String name, Location location, Organizer organizer, int nrTicketTypes, Ticket[] tickets, Date startDate, String homeTeam, String guestTeam) {
         super(name, location, organizer, nrTicketTypes, tickets, startDate);
-        this.refereesName = refereesName;
         this.homeTeam = homeTeam;
         this.guestTeam = guestTeam;
     }
 
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getGuestTeam() {
+        return guestTeam;
+    }
+
+    @Override
+    public void showMainInfo() {
+        super.showMainInfo();
+        System.out.println(homeTeam + " vs " + guestTeam);
+
+
+    }
     public void showRestInfo(){}
-    protected void setTicketsNumber(){}
-    protected void setPrice(){}
+
 }
